6f003b78a01c876e156a3960224119cbdd80f556
Miguel Sanchez-Brunete <mikiok95@gmail.com>
2017-04-13 10:42:17 +0200

    User view finished

--- app/assets/javascripts/users/show.js
+++ app/assets/javascripts/users/show.js
@@ -1,11 +1,14 @@
-$('button').on('click', function(){
-  $('button').removeClass('selected');
+$('button[class*="trips"]').on('click', function(){
+  $('button[class*="trips"]').removeClass('selected');
   var trip = "." + this.className.toString() + "-list";
   $(this).addClass('selected');
   $('ul[class*="list"]').css("display","none");
   $(trip).css("display","block");
 });
 
+document.getElementsByClassName("new-trip")[0].addEventListener('click', function(){
+	$('.new-trip-modal').modal("show");
+});
 document.getElementById('upload-avatar').addEventListener('change', function(event) {
 	var image = event.target.files[0];
   var reader = new FileReader();
